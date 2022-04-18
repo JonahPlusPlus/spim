5ef4d43ea7a3dd1b67d2f3f9fa70e7ab7c2fbf15
Jonah Henriksson <33059163+JonahPlusPlus@users.noreply.github.com>
2022-04-18 19:25:46 -0400

    Added write_register and PC_update
    Also added initialization where necessary

--- project.c
+++ project.c
@@ -45,6 +45,27 @@ int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 /* 10 Points */
 void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec)
 {
+    if (op == NULL)
+        op = malloc(sizeof(unsigned));
+
+    if (r1 == NULL)
+        r1 = malloc(sizeof(unsigned));
+
+    if (r2 == NULL)
+        r2 = malloc(sizeof(unsigned));
+
+    if (r3 == NULL)
+        r3 = malloc(sizeof(unsigned));
+
+    if (funct == NULL)
+        funct = malloc(sizeof(unsigned));
+
+    if (offset == NULL)
+        offset = malloc(sizeof(unsigned));
+
+    if (jsec == NULL)
+        jsec = malloc(sizeof(unsigned));
+
     *op = instruction >> 26;
     *r1 = (instruction >> 21) & 0x1F;
     *r2 = (instruction >> 16) & 0x1F;
@@ -63,13 +84,14 @@ int instruction_decode(unsigned op,struct_controls *controls)
     if (controls == NULL) {
         controls = calloc(1, sizeof(struct_controls));
     } else {
-        memset(controls, 0, sizeof(struct_controls));
+        memset(controls, 0, sizeof(struct_controls)); // Prevents data corruption from previous cycles
     }
 
     switch (op) {
         case 0b000000: // R-format *
             controls->RegDst = 1;
             controls->ALUOp = 0b111;
+            controls->RegWrite = 1;
             break;
         case 0b000010: // jump *
             controls->Jump = 1;
@@ -91,15 +113,13 @@ int instruction_decode(unsigned op,struct_controls *controls)
             break;
         case 0b001111: // load upper immediately *
             controls->MemtoReg = 1;
-            controls->ALUSrc = 1;
+            controls->MemRead = 1;
             break;
         case 0b100011: // load word *
             controls->MemRead = 1;
-            controls->ALUSrc = 1;
             break;
         case 0b101011: // store word *
             controls->MemWrite = 1;
-            controls->ALUSrc = 1;
             break;
         default:
             return 1;
@@ -127,7 +147,17 @@ void sign_extend(unsigned offset,unsigned *extended_value)
 /* 10 Points */
 int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigned funct,char ALUOp,char ALUSrc,unsigned *ALUresult,char *Zero)
 {
-    if (ALUOp < 0b111) {
+    if (ALUresult == NULL)
+        ALUresult = calloc(1, sizeof(unsigned));
+    else
+        memset(ALUresult, 0, sizeof(unsigned)); // Some paths may not call ALU, so we have to prevent corruption
+
+    if (Zero == NULL)
+        Zero = malloc(sizeof(char));
+    else
+        memset(Zero, 0, sizeof(char));
+
+    if (ALUSrc == 1) {
         ALU(data1, extended_value, ALUOp, ALUresult, Zero);
     } else if (ALUOp == 0b111) {
         switch (funct) {
@@ -183,14 +213,33 @@ int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsig
 /* 10 Points */
 void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,char RegWrite,char RegDst,char MemtoReg,unsigned *Reg)
 {
+    if (RegWrite == 1) {
+        if (RegDst == 1) {
+            Reg[r3] = ALUresult;
+        } else {
+            Reg[r2] = ALUresult;
+        }
+    }
 
+    if (MemtoReg == 1) {
+        if (RegDst == 1) {
+            Reg[r3] = memdata;
+        } else {
+            Reg[r2] = memdata;
+        }
+    }
 }
 
 /* PC update */
 /* 10 Points */
 void PC_update(unsigned jsec,unsigned extended_value,char Branch,char Jump,char Zero,unsigned *PC)
 {
-
-    *PC++;
+    if (Jump == 1) {
+        *PC = jsec;
+    } else if (Branch == 1) {
+        
+    } else {
+        *PC++;
+    }
 }
 
